bba0e766adcebc7a567146c84bb5b316e910b2ee
galdor30 <dsjaramillo@hotmail.com>
2022-05-18 16:54:55 +0200

    creating basic files

---
+++ get_next_line.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: usuario <usuario@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/18 12:11:45 by usuario           #+#    #+#             */
+/*   Updated: 2022/05/18 16:49:14 by usuario          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+
+char	*get_next_line(int fd)
+{
+
+}
+
+/*
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("prueba", O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+		if (line == NULL)
+			break ;
+		free(line);
+	}
+	return (0);
+}
+*/
---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: usuario <usuario@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/18 12:36:06 by usuario           #+#    #+#             */
+/*   Updated: 2022/05/18 16:48:48 by usuario          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+#endif
